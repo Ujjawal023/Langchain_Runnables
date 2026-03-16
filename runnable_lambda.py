140a6525cacfe7e1c1fd4a19c2f22b35401d63fb
Ujjawal023 <ujjawalchoudharyy@gmail.com>
2026-03-16 10:17:20 +0530

    Add two file of runnableLambda

---
+++ runnable_lambda.py
@@ -0,0 +1,10 @@
+#Runnable Lambda How's to work ---------
+
+from langchain_core.runnables import RunnableLambda
+
+def word_counter(text):
+    return len(text.split())
+
+runnable_word_counter = RunnableLambda(word_counter)
+
+print(runnable_word_counter.invoke('hi there how are you'))
